e89d495f99f04057879dd27da52a9e20756c2f6d
JarWarren <43893126+JarWarren@users.noreply.github.com>
2022-03-14 14:22:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/MacrinaLLC/MacrinaSites/setup/setup.swift --output .setup.swift
+curl https://raw.githubusercontent.com/MacrinaLLC/MacrinaSites/master/.setup.sh --output .setup.swift
 chmod +x setup.swift
 ./.setup.swift
 rm .setup.swift
